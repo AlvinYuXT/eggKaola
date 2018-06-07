7316aa0dd85faf8ccab9a493ddae6523b4bd3c08
AlvinYuxt <yuxitong_al@163.com>
2018-06-07 20:09:45 +0800

    fix: route error

--- app/router.ts
+++ app/router.ts
@@ -3,6 +3,6 @@ import { Application } from 'egg';
 export default (app: Application) => {
   const { controller, router } = app;
 
-  router.get('/x', controller.home.index);
+  router.get('/', controller.home.index);
   router.get('/api/mall/goods', controller.goods.index);
 };
